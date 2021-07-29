0d78a133b5cc2a4517e99e7bb598229ee3a9f68d
Jorge Lima <jorgefilipemlima@gmail.com>
2021-07-29 20:13:38 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# React + Tailwind CSS
+This is a _kinda_ opinitionated boilerplate for React applications. 🚀
+
+### Available npm commands:
+- Start development environment: `npm run start` or `yarn start`
+- Build and bundle application: `npm run build` or `yarn build`
+
+### Libraries installed:
+- webpack for bundling 📦
+- webpack-dev-server for development runtime 🚀
+- babel for ES5+ and JSX translation 🈶
+- postcss + tailwindcss + autoprefixer 🎨 (already setup with webpack)
+- import aliases
+
+Made by @jfmlima inspired by @jhfgloria [react-boilerplate](https://github.com/jhfgloria/react-boilerplate)
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+const htmlWebPackPlugin = new HtmlWebPackPlugin({
+  template: "./src/index.html",
+  filename: "./index.html"
+});
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.jsx|\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1
+            }
+          },
+          'postcss-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    htmlWebPackPlugin
+  ],
+  resolve: {
+    extensions: ['.js', '.ts'],
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@assets': path.resolve(__dirname, 'src/assets'),
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@containers': path.resolve(__dirname, 'src/containers'),
+    },
+  },
+};
